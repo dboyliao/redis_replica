73cca8b7651b4c08ccadb83a43855e8e3282797e
dboyliao <qmalliao@gmail.com>
2015-04-07 15:37:16 +0800

    Update the version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='redis_replica',
-    version='0.0.3',
+    version='0.0.4',
     description='Redis Replica Set Client',
     author='Yin-Chen Liao',
     author_email='qmalliao@gmail.com',
